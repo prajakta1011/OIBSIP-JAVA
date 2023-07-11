61f23eafe74f84c46faa9d3629f834a67efb1b1a
Prajakta Desai <111192248+prajakta1011@users.noreply.github.com>
2023-07-11 22:09:21 +0530

    Update Atm.java

--- Atm/Atm.java
+++ Atm/Atm.java
@@ -1,1 +1,18 @@
+    private double balance;
+    private double depositAmount;
+    private double withdrawAmount;
+    //default constructor
+    public Atm(){
 
+    }
+    //getter Setter
+
+    public double getBalance()
+    {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}

71d14cdb7c36b6d4b045761fc2aefb4439634d54
Prajakta Desai <111192248+prajakta1011@users.noreply.github.com>
2023-07-11 20:59:12 +0530

    Add files via upload

---
+++ Atm.java
@@ -0,0 +1,19 @@
+public class Atm{
+    private double balance;
+    private double depositAmount;
+    private double withdrawAmount;
+    //default constructor
+    public Atm(){
+
+    }
+    //getter Setter
+
+    public double getBalance()
+    {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
